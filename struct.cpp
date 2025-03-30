8be0930664976fde36711e86966671766d338e29
Bisma <bismajaved1092@gmail.com>
2025-03-30 08:58:18 +0500

    Add files via upload

---
+++ struct.cpp
@@ -0,0 +1,24 @@
+//#include<iostream>
+//using namespace std;
+//struct MyDatatype
+//{
+//	int x;
+//	float y;
+//};
+//struct MyDatatype2
+//{
+//	double x;
+//	char y;
+//	MyDatatype obj1;
+//};
+//
+//int main()
+//{
+//	MyDatatype2 obj2;
+//	obj2.x = 10.9;
+//	obj2.y = 'y';
+//	obj2.obj1.x = 23;
+//	cout << obj2.x << endl;
+//	cout << obj2.y << endl;
+//	cout << obj2.obj1.x << endl;
+//}
